8e456811100dae68e766d532ac9e296f05dbbb46
Matheus Silva <matheussgsilva@hotmail.com>
2023-06-04 19:02:30 -0300

    Config Eslint and Editorconfig

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send('Hello World')
-})
+  response.send('Hello World');
+});
 
-app.listen(3000, () => console.log('🚀 Server started at http://localhost:3000'))
+app.listen(3000, () => console.log('🚀 Server started at http://localhost:3000'));
