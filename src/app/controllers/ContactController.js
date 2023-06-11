9f50d43cba6072e04ce65f4899a3300d7ebc646a
Matheus Silva <matheussgsilva@hotmail.com>
2023-06-11 17:55:08 -0300

    Create the Update Contacts

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -41,8 +41,33 @@ class ContactController {
     response.json(contact);
   }
 
-  update() {
+  async update(request, response) {
+    const { id } = request.params;
+    const {
+      name, email, phone, category_id,
+    } = request.body;
 
+    const contactExists = await ContactsRepository.findById(id);
+
+    if (!contactExists) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contacByEmail = await ContactsRepository.findByEmail(email);
+
+    if (contacByEmail && contacByEmail.id !== id) {
+      return response.status(400).json({ error: 'This e-mail is already in use' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name, email, phone, category_id,
+    });
+
+    response.json(contact);
   }
 
   async delete(request, response) {
