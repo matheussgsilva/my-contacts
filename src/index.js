4d765b68a9c3594f31874680174ff18085926e92
Matheus Silva <matheussgsilva@hotmail.com>
2023-09-17 19:37:09 -0300

    Create the CORS Header and create the middlewares to handler cors and errors

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ const express = require('express');
 // eslint-disable-next-line import/no-extraneous-dependencies
 require('express-async-errors');
 
+const cors = require('./app/middlewares/cors');
+const errors = require('./app/middlewares/errorHandler');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 app.use(routes);
-app.use((error, request, response, next) => {
-  console.log(error);
-  response.sendStatus(500);
-});
+app.use(errors);
 
-app.listen(3000, () => console.log('🚀 Server started at http://localhost:3000'));
+app.listen(3001, () => console.log('🚀 Server started at http://localhost:3001'));
